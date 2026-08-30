9b615967cba074326a98a7190e2caa9769c36028
blessedresults <blessedresult6@gmail.com>
2026-08-30 17:36:26 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,6 @@
 // app/page.tsx
-'use client';
-
-import { useEffect } from 'react';
+import { redirect } from 'next/navigation';
 
 export default function HomePage() {
-  useEffect(() => {
-    // Redirect to index.html
-    window.location.href = '/index.html';
-  }, []);
-
-  return (
-    <div style={{ textAlign: 'center', padding: '50px' }}>
-      <p>Redirecting to login...</p>
-    </div>
-  );
+  redirect('/login');
 }
